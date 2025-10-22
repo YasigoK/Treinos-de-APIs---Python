97d01c9b9d66e0c62074fcb5064cbe1124efcfcd
YasigoK <yago.kenji99@gmail.com>
2025-10-22 15:45:44 -0300

    Melhorando visual  + variação de temperatura

--- API_Previsao/previsao.py
+++ API_Previsao/previsao.py
@@ -4,8 +4,7 @@ import requests
 import pprint
 import os
 import streamlit as st
-
-os.system('cls')
+import numpy as np
 
 def get_previsao(id):
     url =f"http://servicos.cptec.inpe.br/XML/cidade/{id}/previsao.xml"
@@ -17,38 +16,66 @@ def get_previsao(id):
         return dados_cidade
     else:
         print("Erro ao acessar informação")
+        return None
 
-
+st.set_page_config(layout="wide")
 
 st.title("previsão de 4 dias")
 entrada = st.text_input("Digite qual cidade quer saber a previsão",value="241")
 
 previsao = get_previsao(entrada)
 
-#pp = pprint.PrettyPrinter(indent=2)
 nome_cidade = previsao['cidade']['nome']
 previsao4dias = previsao['cidade']['previsao']
-#pp.pprint(previsao4dias)
-
 st.header(f"Previsão para : {nome_cidade}")
-#print("-="*40)
-for repet in previsao4dias:
-    with st.container(border=True):
+
+num_dias = len(previsao4dias)
+colunas = st.columns(num_dias)
+atual_max = np.zeros(num_dias)
+atual_min = np.zeros(num_dias)
+for i, repet in enumerate(previsao4dias):
+
+    with colunas[i]:
         dia = repet.get('dia', 'N/A')
         iuv = repet.get('iuv', 'N/A')
         maxima = repet.get('maxima', 'N/A')
         minima = repet.get('minima', 'N/A')
         tempo = repet.get('tempo', 'N/A')
+        atual_min[i] = float(minima)
+        atual_max[i] = float(maxima)
+        if i == 0:
+            variacao_min = None 
+            variacao_max = None
+        else:
+            variacao_min = atual_min[i] - atual_min[i-1]
+            variacao_max = atual_max[i] - atual_max[i-1]
+  
+        with st.container(border=True):
+                
+                st.markdown(f"### PREVISÃO PARA O DIA :\n  ### **{dia}**")
 
-        st.markdown(f"### PREVISÃO PARA O DIA : **{dia}**")
+                temperatura1,temperatura2,descricao = st.columns(3)
+                with temperatura1:
+                    st.metric(
+                        label="Temperatura Min",
+                        value=f"{minima}°C",
+                        delta=f"{variacao_min:+.1f}" if variacao_min is not None else None, 
+                        delta_color="inverse",
+                    )
+                with temperatura2:
+                    st.metric(
+                        label="Temperatura Max",
+                        value=f"{maxima}°C",
+                        delta=f"{variacao_min:+.1f}" if variacao_min is not None else None, 
+                        delta_color="inverse",
+                    )
+            
+                with descricao:
+                    st.metric(
+                        label="Tempo",
+                        value=f"{tempo}",
+                    )
 
-        temperatura,descricao = st.columns(2)
-        with temperatura:
-            st.metric(
-                label="Temperatura (min/max)",
-                value=f"{minima}°C   {maxima}°C {iuv} {tempo}" ,
-            )
-        
 
 
 
